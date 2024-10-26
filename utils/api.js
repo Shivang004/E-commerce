d6fca37f4dcbf7761ec1c1e17a40820b95deb251
Shivang Agarwal <72720903+Shivang004@users.noreply.github.com>
2024-10-26 15:53:45 +0530

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://e-commerce-silk-iota-15.vercel.app/' || 'http://localhost:3000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; || 'http://localhost:3000';
 
 export const getProducts = async () => {
   const response = await axios.get(`${API_BASE_URL}/products`);
