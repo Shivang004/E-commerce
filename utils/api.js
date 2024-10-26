1e48d127b3c38673a5d7ff6027d1588580edf9d7
Shivang Agarwal <72720903+Shivang004@users.noreply.github.com>
2024-10-26 11:33:50 +0530

    Create api.js

---
+++ utils/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+export const getProducts = async () => {
+  const response = await axios.get(`${API_BASE_URL}/products`);
+  return response.data;
+};
+
+export const addProduct = async (product) => {
+  const response = await axios.post(`${API_BASE_URL}/products`, product);
+  return response.data;
+};
+
+export const updateProduct = async (id, product, token) => {
+  const response = await axios.put(`${API_BASE_URL}/products/${id}`, product, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const deleteProduct = async (id, token) => {
+  const response = await axios.delete(`${API_BASE_URL}/products/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const loginUser = async (credentials) => {
+  const response = await axios.post(`${API_BASE_URL}/auth/login`, credentials);
+  return response.data;
+};
